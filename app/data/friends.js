657a8d0720cb1aa7e288b447a56954b3c3672193
Kyle Hoernlein <khoernlein90@gmail.com>
2017-10-25 18:33:46 -0400

    updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,15 +18,15 @@ var friendArray = [{
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         5,
-        1,
+        5,
         4,
         4,
         5,
-        1,
+        5,
         2,
         5,
         4,
-        1
+        5
     ]
 }, ]
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,7 +2,32 @@ var friendData = require("../data/friends");
 
 module.exports = function(app) {
 
-    app.get("/api/friendList", function(req, res) {
+    app.get("/api/friends", function(req, res) {
         res.json(friendData);
+        console.log(friendData)
     });
-};
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        res.json(newFriend)
+        console.log(newFriend)
+
+        for (var i = 0; i < newFriend.scores.length; i++) {
+        	parseInt(newFriend.scores[i])
+        	console.log(newFriend.scores[i])
+        }
+
+        for (var i = 0; i < friendData.length; i++) {
+            console.log(friendData[i].scores);
+            // var numbers = parseInt(friendData[i].scores)
+            var sum = parseInt(friendData[i].scores.reduce(add, 0));
+            function add(a, b) {
+                return a + b;
+            }
+            console.log(sum);
+
+        }
+        friendData.push(newFriend);
+
+    })
+
+}
