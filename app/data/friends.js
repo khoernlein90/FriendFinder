4e77dc3b082f3d66319627531f8911002929d16a
Kyle Hoernlein <khoernlein90@gmail.com>
2017-10-27 19:09:28 -0400

    more updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendArray = [{
-    "name": "Ahmed",
-    "photo": "https://s-media-cache-ak0.pinimg.com/originals/88/82/26/888226db4863dbed92aefc00cdf65d77.gif",
+    "name": "Tyrion Lannister",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg",
     "scores": [
         3,
         4,
@@ -14,8 +14,8 @@ var friendArray = [{
         4
     ]
 }, {
-    "name": "John",
-    "photo": "https://pbs.twimg.com/profile_images/378800000127303169/07b399286f12b56a651ff8a3c2b2375e.jpeg",
+    "name": "Cersei Lannister",
+    "photo": "https://pbs.twimg.com/profile_images/378800000190054715/421f9de2ef79dbc7c7a55d8ef058fafc.png",
     "scores": [
         2,
         2,
@@ -29,8 +29,8 @@ var friendArray = [{
         1
     ]
 }, {
-    "name": "Kyle",
-    "photo": "https://pbs.twimg.com/profile_images/378800000506299771/f0a5daeb9f9279b6d2ea6b14d4712466.jpeg",
+    "name": "Jon Snow",
+    "photo": "https://pbs.twimg.com/profile_images/3456602315/aad436e6fab77ef4098c7a5b86cac8e3.jpeg",
     "scores": [
         3,
         1,
@@ -44,8 +44,8 @@ var friendArray = [{
         5
     ]
 },  {
-    "name": "Dylan",
-    "photo": "https://pbs.twimg.com/profile_images/3701607243/1a82215dbe91eb70a9a4adb262dadfd8.jpeg",
+    "name": "Joffrey Baratheon",
+    "photo": "http://www.totallytimelines.com/wp-content/uploads/2016/09/JoffreyBaratheon.jpg",
     "scores": [
         5,
         1,
