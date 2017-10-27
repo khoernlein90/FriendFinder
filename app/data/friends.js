3d734f9f614fd51ace7e574de515e0bc400db049
Kyle Hoernlein <khoernlein90@gmail.com>
2017-10-27 18:50:37 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,63 @@
 var friendArray = [{
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://s-media-cache-ak0.pinimg.com/originals/88/82/26/888226db4863dbed92aefc00cdf65d77.gif",
     "scores": [
-        5,
-        1,
+        3,
         4,
-        4,
-        5,
         1,
         2,
         5,
+        5,
+        1,
+        1,
+        2,
+        4
+    ]
+}, {
+    "name": "John",
+    "photo": "https://pbs.twimg.com/profile_images/378800000127303169/07b399286f12b56a651ff8a3c2b2375e.jpeg",
+    "scores": [
+        2,
+        2,
+        4,
+        2,
+        5,
+        3,
+        2,
+        4,
         4,
         1
     ]
 }, {
-    "name": "guy",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Kyle",
+    "photo": "https://pbs.twimg.com/profile_images/378800000506299771/f0a5daeb9f9279b6d2ea6b14d4712466.jpeg",
+    "scores": [
+        3,
+        1,
+        1,
+        3,
+        5,
+        5,
+        5,
+        4,
+        3,
+        5
+    ]
+},  {
+    "name": "Dylan",
+    "photo": "https://pbs.twimg.com/profile_images/3701607243/1a82215dbe91eb70a9a4adb262dadfd8.jpeg",
     "scores": [
         5,
-        5,
-        4,
-        4,
-        5,
+        1,
+        1,
         5,
         2,
+        3,
+        4,
         5,
         4,
         5
     ]
-}, ]
+} ]
 
 module.exports = friendArray;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -4,30 +4,37 @@ module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
         res.json(friendData);
-        console.log(friendData)
     });
+
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-        res.json(newFriend)
-        console.log(newFriend)
-
-        for (var i = 0; i < newFriend.scores.length; i++) {
-        	parseInt(newFriend.scores[i])
-        	console.log(newFriend.scores[i])
-        }
-
-        for (var i = 0; i < friendData.length; i++) {
-            console.log(friendData[i].scores);
-            // var numbers = parseInt(friendData[i].scores)
-            var sum = parseInt(friendData[i].scores.reduce(add, 0));
-            function add(a, b) {
-                return a + b;
-            }
-            console.log(sum);
-
-        }
         friendData.push(newFriend);
 
-    })
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
 
+        var newFriendScores = newFriend.scores;
+        var difference = 0;
+
+        for (var i = 0; i < (friendData.length - 1); i++) {
+            var oldFriend = friendData[i];
+            difference = 0;
+
+            for (var j = 0; j < oldFriend.scores.length; j++) {
+                var oldFriendScore = oldFriend.scores[j];
+                var currentUserScore = newFriendScores[j];
+
+                difference += Math.abs(parseInt(currentUserScore) - parseInt(oldFriendScore));
+            }
+            if (difference <= bestMatch.friendDifference) {
+                bestMatch.name = oldFriend.name;
+                bestMatch.photo = oldFriend.photo;
+                bestMatch.friendDifference = difference;
+            }
+        }
+        res.json(bestMatch);
+    });
 }
